a5c5c1cf6dd242abd034c86fe42b40d437bed18e
madummuhammad <muhammad.madum2018@gmail.com>
2021-07-15 22:17:42 +0700

    Mengedit gambar yang sebelumnya tidak muncul di produk selengkapnya

--- application/views/company_profile/product/v_more.php
+++ application/views/company_profile/product/v_more.php
@@ -1,5 +1,5 @@
 <!-- Masthead-->
-<header class="masthead" style="background-image: url(http://localhost/admin_cococreative/assets/assets/img/upload/produk/<?php echo $this->M_Produk->tampil_top_img_view_more()['gambar'] ?>)">
+<header class="masthead" style="background-image: url(<?php echo base_url('main') ?>/assets/assets/img/upload/produk/<?php echo $this->M_Produk->tampil_top_img_view_more()['gambar'] ?>)">
 	<div class="container">
 		<h1 class="text-uppercase"><?php echo $this->M_Produk->tampil_top_img_view_more()['nama_jenis_produk'] ?></h1>
 	</div>
@@ -10,7 +10,7 @@
 		<div class="row text-center">
 			<?php foreach ($view_more as $row => $value): ?>
 			<div class="col-md-6 mt-2">
-				<img class="img-fluid" src="http://localhost/admin_cococreative/assets/assets/img/upload/variasi_produk/<?php echo $value['gambar_variasi'] ?>" alt="" />
+				<img class="img-fluid" src="<?php echo base_url('main') ?>/assets/assets/img/upload/variasi_produk/<?php echo $value['gambar_variasi'] ?>" alt="" />
 				<h4 class="my-3"><?php echo $value['isi_pilihan_ukuran'] ?></h4>
 				<p class="mb-0 text-muted text-justify keterangan-produk"><?php echo $value['keterangan_variasi_produk'] ?></p>
 				<button class="btn btn-primary mt-3 btn-lg m-2" data-target="#vm<?php echo $value['id_pilihan_ukuran'] ?>" data-toggle="modal">ADD TO CHART <i class="fas fa-shopping-cart"></i></button>
