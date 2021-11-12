617db14dcf70a4f8643db735e55acb00e9e0e0ab
madummuhammad <muhammad.madum2018@gmail.com>
2021-11-12 11:40:20 +0700

    memperbaiki navigasi

--- application/views/company_profile/partial/mainNav.php
+++ application/views/company_profile/partial/mainNav.php
@@ -14,10 +14,18 @@
                 <nav class="navbar navbar-expand-lg navbar-light p-0">
 
                   <div class="logo custom_logo">
-                    <a class="d-flex justify-content-between align-items-center" href="">
-                      <img loading="lazy" class="rounded-circle" src="<?= base_url('assets/') ?>images/logo.png" alt="Constra">
-                      <h4 class="pl-2">Cococreative Indonesia</h4>
-                    </a>
+                    <?php if ($this->uri->segment(1)=='teams'): ?>
+                      <a class="d-flex justify-content-between align-items-center" href="<?= base_url() ?>">
+                        <img loading="lazy" class="rounded-circle" src="<?= base_url('assets/') ?>images/logo.png" alt="Constra">
+                        <h4 class="pl-2">Cococreative Indonesia</h4>
+                      </a>
+                    <?php else: ?>
+                      <a class="d-flex justify-content-between align-items-center" href="">
+                        <img loading="lazy" class="rounded-circle" src="<?= base_url('assets/') ?>images/logo.png" alt="Constra">
+                        <h4 class="pl-2">Cococreative Indonesia</h4>
+                      </a>
+                    <?php endif ?>
+
                   </div><!-- logo end -->
 
                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,39 +34,55 @@
 
                   <div id="navbar-collapse" class="collapse navbar-collapse">
                     <?php if(get_cookie('lang_is') === 'in'){ ?>
-                      <ul class="nav navbar-nav ml-auto align-items-center">
-                        <li class="nav-item"><a class="nav-link btn-navbar" data-target="body">Beranda</a></li>
-                        <li class="nav-item"><a class="nav-link btn-navbar" data-target="#ts-features">Fitur</a></li>
-                        <li class="nav-item"><a class="nav-link btn-navbar" data-target="#product">Produk</a></li>
-                        <li class="nav-item"><a class="nav-link btn-navbar" data-target="#about-us">Tentang Kami</a></li>
-                        <li class="nav-item"><a class="nav-link btn-navbar" data-target="#galery">Galeri</a></li>
-                        <li class="nav-item"><a href="<?= base_url('teams') ?>" class="nav-link btn-navbar">Tim</a></li>
-                        <li class="nav-item"><a class="nav-link btn-navbar" data-target="#footer">Kontak</a></li>
-                        <li class="nav-item dropdown">
-                          <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"><img src="icon/translete_icon.svg" alt="">ID <i class="fa fa-angle-down"></i></a>
-                          <ul class="dropdown-menu" role="menu">
-                            <li><a href="<?php echo site_url('lang_setter/set_to/indonesia');?>">ID</a></li>
-                            <li><a href="<?php echo site_url('lang_setter/set_to/english');?>">ENG</a></li>
-                          </ul>
-                        </li>
-                      </ul>
+                      <?php if ($this->uri->segment(1)=='teams'): ?>
+                        <ul class="nav navbar-nav ml-auto align-items-center">
+                          <li class="nav-item"><a class="nav-link btn-navbar" href="<?= base_url() ?>" data-target="body">Beranda</a></li>
+                          <li class="nav-item active"><a href="<?= base_url('teams') ?>" class="nav-link btn-navbar">Tim</a></li>
+                        </ul>
+                      <?php else: ?>
+                        <ul class="nav navbar-nav ml-auto align-items-center">
+                          <li class="nav-item"><a class="nav-link btn-navbar" data-target="body">Beranda</a></li>
+                          <li class="nav-item"><a class="nav-link btn-navbar" data-target="#ts-features">Fitur</a></li>
+                          <li class="nav-item"><a class="nav-link btn-navbar" data-target="#product">Produk</a></li>
+                          <li class="nav-item"><a class="nav-link btn-navbar" data-target="#about-us">Tentang Kami</a></li>
+                          <li class="nav-item"><a class="nav-link btn-navbar" data-target="#galery">Galeri</a></li>
+                          <li class="nav-item"><a href="<?= base_url('teams') ?>" class="nav-link btn-navbar">Tim</a></li>
+                          <li class="nav-item"><a class="nav-link btn-navbar" data-target="#footer">Kontak</a></li>
+                          <li class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"><img src="icon/translete_icon.svg" alt="">ID <i class="fa fa-angle-down"></i></a>
+                            <ul class="dropdown-menu" role="menu">
+                              <li><a href="<?php echo site_url('lang_setter/set_to/indonesia');?>">ID</a></li>
+                              <li><a href="<?php echo site_url('lang_setter/set_to/english');?>">ENG</a></li>
+                            </ul>
+                          </li>
+                        </ul>
+                      <?php endif ?>
+                      
                     <?php } else { ?>
-                      <ul class="nav navbar-nav ml-auto align-items-center">
-                        <li class="nav-item"><a class="nav-link btn-navbar" data-target="body">Home</a></li>
-                        <li class="nav-item"><a class="nav-link btn-navbar" data-target="#ts-features">Feature</a></li>
-                        <li class="nav-item"><a class="nav-link btn-navbar" data-target="#product">Product</a></li>
-                        <li class="nav-item"><a class="nav-link btn-navbar" data-target="#about-us">About</a></li>
-                        <li class="nav-item"><a class="nav-link btn-navbar" data-target="#galery">Galery</a></li>
-                        <li class="nav-item"><a href="<?= base_url('teams') ?>" class="nav-link btn-navbar">Teams</a></li>
-                        <li class="nav-item"><a class="nav-link btn-navbar" data-target="#footer">Contact</a></li>
-                        <li class="nav-item dropdown">
-                          <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"><img src="icon/translete_icon.svg" alt="">ENG <i class="fa fa-angle-down"></i></a>
-                          <ul class="dropdown-menu" role="menu">
-                            <li><a href="<?php echo site_url('lang_setter/set_to/indonesia');?>">ID</a></li>
-                            <li><a href="<?php echo site_url('lang_setter/set_to/english');?>">ENG</a></li>
-                          </ul>
-                        </li>
-                      </ul>
+                      <?php if ($this->uri->segment(1)=='teams'): ?>
+                        <ul class="nav navbar-nav ml-auto align-items-center">
+                          <li class="nav-item"><a class="nav-link btn-navbar" data-target="body">Home</a></li>
+                          <li class="nav-item active"><a href="<?= base_url('teams') ?>" class="nav-link btn-navbar">Teams</a></li>
+                        </ul>
+                      <?php else: ?>
+                        <ul class="nav navbar-nav ml-auto align-items-center">
+                          <li class="nav-item"><a class="nav-link btn-navbar" data-target="body">Home</a></li>
+                          <li class="nav-item"><a class="nav-link btn-navbar" data-target="#ts-features">Feature</a></li>
+                          <li class="nav-item"><a class="nav-link btn-navbar" data-target="#product">Product</a></li>
+                          <li class="nav-item"><a class="nav-link btn-navbar" data-target="#about-us">About</a></li>
+                          <li class="nav-item"><a class="nav-link btn-navbar" data-target="#galery">Galery</a></li>
+                          <li class="nav-item"><a href="<?= base_url('teams') ?>" class="nav-link btn-navbar">Teams</a></li>
+                          <li class="nav-item"><a class="nav-link btn-navbar" data-target="#footer">Contact</a></li>
+                          <li class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown"><img src="icon/translete_icon.svg" alt="">ENG <i class="fa fa-angle-down"></i></a>
+                            <ul class="dropdown-menu" role="menu">
+                              <li><a href="<?php echo site_url('lang_setter/set_to/indonesia');?>">ID</a></li>
+                              <li><a href="<?php echo site_url('lang_setter/set_to/english');?>">ENG</a></li>
+                            </ul>
+                          </li>
+                        </ul>
+                      <?php endif ?>
+                      
                     <?php } ?>
                   </div>
                 </nav>
